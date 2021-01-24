937fc0cb27ae12e63de8696d0f500ab78ae29d70
Gagadok <51823702+Gagadok@users.noreply.github.com>
2021-01-24 15:10:59 +0300

    Delete EYQ1 directory

--- EYQ1/CreateTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Information(
-  ID INT identity (0,1) NOT NULL,
-  date DATE NOT NULL,
-  latin_characters CHAR (10) NOT NULL,
-  russian_characters NCHAR (10) NOT NULL,
-  integer int NOT NULL,
-  real DECIMAL (18, 10) NOT NULL, 
-  PRIMARY KEY (ID)
-);
